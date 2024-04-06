d2bf3ed3731bb3b8abb7c594381b8539004f099f
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2024-04-06 16:42:55 -0300

    feat: Criação de datas atualizações visuais e de funcionalidades nos buttons

--- pass_in_web/src/components/attendee_list.tsx
+++ pass_in_web/src/components/attendee_list.tsx
@@ -11,8 +11,42 @@ import { Table } from "./table/table";
 import { TableHeader } from "./table/table_header";
 import { TableCell } from "./table/table_cell";
 import { TableRow } from "./table/table-row";
+import { ChangeEvent, useState } from "react";
+import { attendees } from "./data/attendees";
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br'
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+dayjs.locale('pt-br')
 
 export function AttendeeList() {
+
+  const [ search, setsearch ] = useState('')
+  const [page, setPage] = useState(1)
+  const totalPages = Math.ceil(attendees.length / 10)
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+    setsearch(event.target.value);
+  }
+  
+  function goToFirstPage() {
+    setPage(1)
+  }
+
+  function goToLastPage() {
+    setPage (totalPages)
+  }
+
+  function goToNextPage() {
+    setPage(page + 1)
+  }
+
+  function goToPreviusPage() {
+    setPage(page - 1)
+  }
+
+
   return (
     <>
       <div className="flex flex-col gap-4">
@@ -20,11 +54,13 @@ export function AttendeeList() {
           <h1 className="text-2xl font-bold">Participantes</h1>
           <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
             <Search className="size-4 text-emerald-300" />
-            <input
+            <input onChange={onSearchInputChanged}
               className="bg-transparent flex-1 outline-none h-auto border-0 p-0 text-sm"
               placeholder="Buscar Participante"
             />
           </div>
+
+          {search}
         </div>
 
         <Table>
@@ -41,21 +77,22 @@ export function AttendeeList() {
               </tr>
             </thead>
             <tbody>
-              {Array.from({ length: 8 }).map((_, i) => {
+                {attendees.slice((page - 1) * 10, page * 10  ).map((attendee) => {
                 return (
-                  <TableRow key={i} className="border-b border-white/10 hover:bg-white/5">
+                  <TableRow key={attendee.id} className="border-b border-white/10 hover:bg-white/5">
                     <TableCell>
                       <input type="checkbox"  className="size-4 bg-black/20 rounded border border-white/10 "/>
                     </TableCell>
-                    <TableCell>12345</TableCell>
+                    <TableCell>{attendee.id}</TableCell>
                     <TableCell>
                       <div className="flex flex-col gap-1">
-                        <span className="font-semibold text-white">Eduardo da Silva Sichelero</span>
-                        <span>eduardoadf@hotad.com</span>
+                        <span className="font-semibold text-white">{attendee.name}</span>
+                        <span>{attendee.email}</span>
                       </div>
                     </TableCell>
-                    <TableCell> 7 dias atrás</TableCell>
-                    <TableCell>3 dias atrás</TableCell>
+                    <TableCell>{dayjs().to(attendee.createdAT)}</TableCell>
+                    <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+                    
                     <TableCell><IconButton transparent><MoreHorizontal className="size-4" /></IconButton></TableCell>
                   </TableRow>
                 );
@@ -64,27 +101,28 @@ export function AttendeeList() {
 
             <tfoot>
               <tr>
-                <TableCell colSpan={3}>Mostrando 10 de 228 itens</TableCell>
+                <TableCell colSpan={3}>Mostrando 10 de {attendees.length}</TableCell>
                 <TableCell colSpan={3}>
                   <div className="w-full flex justify-end">
                     <div className="inline-flex items-center gap-8">
-                      <span>Página 1 de 23</span>
+                      <span>Página {page} de {totalPages}</span>
                       <div className="flex gap-1.5">
-                        <IconButton>
+                        <IconButton onClick={goToFirstPage} disabled={page === 1}>
                           <ChevronsLeft className="size-4" />
                         </IconButton>
-                        <IconButton>
+
+                        <IconButton onClick={goToPreviusPage} disabled={page === 1}>
                           <ChevronLeft className="size-4" />
                         </IconButton>
-                        <IconButton>
-                          <MoreHorizontal className="size-4" />
-                        </IconButton>
-                        <IconButton>
+
+                        <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                           <ChevronRight className="size-4" />
                         </IconButton>
-                        <IconButton>
+
+                        <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                           <ChevronsRight className="size-4" />
                         </IconButton>
+
                       </div>
                     </div>
                   </div>
